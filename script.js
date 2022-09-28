41b970805d2afe675c32513a60631ed83478d940
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-09-28 12:30:03 -0700

    page structure and input box

---
+++ script.js
@@ -0,0 +1,9 @@
+function userChoice() {
+    var userInput = window.prompt("Piedra, Papel, Tijeras?");
+    // if (userInput="Piedra, Papel, Tijeras");
+    // return userInput;
+    //  else {
+    //     alert("Try Again");
+    //  }
+}
+console.log ("helloworld");
